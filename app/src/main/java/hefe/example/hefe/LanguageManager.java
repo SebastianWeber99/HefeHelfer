9a423cbb9e497f14d38cd34015abfd8d921d851a
SebastianWeber99 <sebastian.m.weber@web.de>
2023-08-14 23:29:04 +0200

    most enormous changes language fix probably???

--- app/src/main/java/hefe/example/hefe/LanguageManager.java
+++ app/src/main/java/hefe/example/hefe/LanguageManager.java
@@ -4,20 +4,29 @@ import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.os.LocaleListCompat;
+
 import java.util.Locale;
 
-public class LanguageManager {
+public class LanguageManager extends AppCompatActivity {
     private Context ct;
     public LanguageManager (Context ctx){
         ct = ctx;
     }
     public void updateResource(String code){
-        Locale locale = new Locale(code);
-        Locale.setDefault(locale);
-        Resources resources = ct.getResources();
-        Configuration configuration = resources.getConfiguration();
-        configuration.setLocale(locale);
-        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+//        Locale locale = new Locale(code);
+//        Locale.setDefault(locale);
+//        Resources resources = ct.getResources();
+//        Configuration configuration = resources.getConfiguration();
+//        configuration.setLocale(locale);
+//        resources.updateConfiguration(configuration, resources.getDisplayMetrics());
+
+
+        LocaleListCompat appLocale = LocaleListCompat.forLanguageTags(code);
+        // Call this on the main thread as it may require Activity.restart()
+        AppCompatDelegate.setApplicationLocales(appLocale);
 
 
     }
