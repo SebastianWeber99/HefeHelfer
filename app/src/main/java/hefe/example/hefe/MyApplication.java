6155e132582f262d4362263c7c73e71f2a217088
cha0smichi <michidoser@gmx.de>
2023-09-17 12:55:47 +0200

    joa iwie läufts nicht so ganz. Ids nochaml neu gemacht

--- app/src/main/java/hefe/example/hefe/MyApplication.java
+++ app/src/main/java/hefe/example/hefe/MyApplication.java
@@ -148,7 +148,7 @@ public class MyApplication extends Application implements Application.ActivityLi
 
         /** Check if ad exists and can be shown. */
         private boolean isAdAvailable() {
-            return appOpenAd != null && wasLoadTimeLessThanNHoursAgo(4);
+            return appOpenAd != null && wasLoadTimeLessThanNHoursAgo(0);
         }
     }
     /** Utility method to check if ad was loaded more than n hours ago. */
